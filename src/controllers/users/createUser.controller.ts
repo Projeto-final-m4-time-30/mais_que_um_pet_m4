2c52aa3e382d3cab247a9cdb7c77da71194f2cc1
Makson <maksonsantoss4@gmail.com>
2022-11-01 18:26:12 -0300

    feat: finalizada as rotas de criação de usuário e login

---
+++ src/controllers/users/createUser.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+import { IUser } from "../../interfaces";
+import { createUserService } from "../../services/users/createUser.service";
+
+export const createUserController = async (req: Request, res: Response) => {
+  const reqBody: IUser = req.validatedBody;
+  const newUser = await createUserService(reqBody);
+  return res.status(201).json(newUser);
+};
--- src/routes/users/user.routes.ts
+++ src/routes/users/user.routes.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
+import { createUserController } from "../../controllers/users/createUser.controller";
+import { loginUserController } from "../../controllers/users/loginUser.controller";
+import { userCreateScheama } from "../../schemas";
+import { createUserSerializer } from "../../serializers/createUser.serializer";
 
 const routes = Router();
 
 export const userRoutes = () => {
-  routes.get("/users");
+  routes.post(
+    "/users",
+    createUserSerializer(userCreateScheama),
+    createUserController
+  );
+
+  routes.post("/login", loginUserController);
 
   return routes;
 };
