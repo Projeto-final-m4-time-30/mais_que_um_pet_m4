b0036b216b71784922779bb2ebacb19a09bf2cba
joaolucasgozzi <lukasgozzi@gmail.com>
2022-11-08 12:27:08 -0400

    fix: new adopt route completed

--- src/services/pets/adoptPet.service.ts
+++ src/services/pets/adoptPet.service.ts
@@ -2,10 +2,9 @@ import AppDataSource from "../../database/data-source";
 import { Pet } from "../../entities/pet.entity";
 import { User } from "../../entities/user.entity";
 import { AppError } from "../../errors/appError";
-import tokenDecoder from "../../utilities/tokenDecoder.utility";
 
 const adoptPetService = async (
-  userToken: string,
+  reqEmail: string,
   petId: string,
   userRegister: string
 ) => {
@@ -13,44 +12,41 @@ const adoptPetService = async (
 
   const petDatabase = AppDataSource.getRepository(Pet);
 
-  const pets = await petDatabase.find();
+  const newOwner = await userDatabase.findOneBy({ email: reqEmail });
 
-  const petExist = pets.find((pet) => {
-    return pet.id === petId;
+  const pet = await petDatabase.findOneBy({
+    user_register: userRegister,
+    id: petId,
   });
 
-  if (!petExist) {
-    throw new AppError("Pet not found.");
+  //Validações
+  if (!newOwner) {
+    throw new AppError("user not found.", 404);
   }
-
-  if (!petExist.is_adoptable || !petExist.is_active) {
-    throw new AppError("Can't adopt this pet.");
+  if (newOwner.id === userRegister) {
+    throw new AppError("Can't adopt own pet.", 401);
   }
-
-  if (!(petExist.user_register === userRegister)) {
-    throw new AppError("You do not have permission");
+  if (!pet) {
+    throw new AppError("Pet not found.", 404);
   }
-
-  const userData: any = tokenDecoder(userToken);
-
-  const user = await userDatabase.findOneBy({ id: userData.user.id });
-
-  if (!user) {
-    throw new AppError("User not found.");
+  if (!pet.is_adoptable || !pet.is_active) {
+    throw new AppError("Can't adopt this pet.", 401);
   }
 
   await petDatabase.update(petId, {
     is_adoptable: false,
-    user: user,
+    user: newOwner,
   });
 
-  const newUser = await userDatabase.findOneBy({ id: userData.id });
-  const newPet = await petDatabase.findOneBy({ id: petExist.id });
+  const updatedUser = await userDatabase.findOneBy({ id: newOwner.id });
 
   return {
     message: "Pet Adopted",
-    user: newUser!.user_name,
-    pet: { name: newPet!.name, is_adoptable: newPet!.is_adoptable },
+    user: {
+      name: updatedUser!.user_name,
+      id: updatedUser!.id,
+      pets: updatedUser!.pets,
+    },
   };
 };
 export default adoptPetService;
